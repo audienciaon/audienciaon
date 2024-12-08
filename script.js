b548160330c7cbf650e93a8e6dd8b0367daf7034
audienciaon <audienciaon@gmail.com>
2024-12-08 19:48:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<script>
-  function incluirArquivo(url, elementoID) {
+function incluirArquivo(url, elementoID) {
     fetch(url)
         .then(response => response.text())
         .then(data => {
@@ -10,5 +9,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     incluirArquivo('header.html', 'header');
     incluirArquivo('footer.html', 'footer');
+
+    // Captura o nome do arquivo atual da URL
+    const paginaAtual = window.location.pathname.split('/').pop();
+    
+    // Se a página não for index.html, carrega o conteúdo da página dentro de /on/
+    if (paginaAtual !== "index.html") {
+        const conteudoUrl = '/on/' + paginaAtual;
+        incluirArquivo(conteudoUrl, 'conteudo-principal');
+    }
 });
-</script>
